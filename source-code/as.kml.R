7070782d8e64806e97d9c4a70c26e1aa6edfb279
Bvpboy <your@email.com>
2025-05-14 23:06:55 -0400

    Added my VERY early shiny app code. Need to work on it a lot more before I turn it on.

--- source-code/as.kml.R
+++ source-code/as.kml.R
@@ -708,24 +708,24 @@ as.kml <- function(animals = list(),
   writeLines(kml_output, "test.kml")
 }
 
-data("buffalo")
-Mvubu <- buffalo$Mvubu
-Cillia <- buffalo$Cilla
-Pepper <- buffalo$Pepper
-Mvubu <- Mvubu[180:200,]
-Cillia <- Cillia[180:200,]
-Pepper <- Pepper[180:200,]
-GUESS <- ctmm.guess(Cillia, interactive = FALSE)
-FIT <- ctmm.fit(Cillia, GUESS, trace = TRUE)
-GUESS2 <- ctmm.guess(Mvubu, interactive = FALSE)
-FIT2 <- ctmm.fit(Mvubu, GUESS2, trace = TRUE)
-as.kml(CTMM = list(FIT,FIT2),
-       all_tour = TRUE,
-       simulation_icons = TRUE,
-       error_circle = TRUE,
-       central_cam = TRUE,
-       pov_cam = FALSE,
-       follow_cam = TRUE,
-       duration = 60, 
-       animals = list(Cillia,Mvubu), 
-       num_simulations = 10)
+# data("buffalo")
+# Mvubu <- buffalo$Mvubu
+# Cillia <- buffalo$Cilla
+# Pepper <- buffalo$Pepper
+# Mvubu <- Mvubu[180:200,]
+# Cillia <- Cillia[180:200,]
+# Pepper <- Pepper[180:200,]
+# GUESS <- ctmm.guess(Cillia, interactive = FALSE)
+# FIT <- ctmm.fit(Cillia, GUESS, trace = TRUE)
+# GUESS2 <- ctmm.guess(Mvubu, interactive = FALSE)
+# FIT2 <- ctmm.fit(Mvubu, GUESS2, trace = TRUE)
+# as.kml(CTMM = list(FIT,FIT2),
+#        all_tour = TRUE,
+#        simulation_icons = TRUE,
+#        error_circle = TRUE,
+#        central_cam = TRUE,
+#        pov_cam = FALSE,
+#        follow_cam = TRUE,
+#        duration = 60, 
+#        animals = list(Cillia,Mvubu), 
+#        num_simulations = 10)
